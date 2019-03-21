9dcf8708dd81dcfb7811954a222344456b769244
Chris Ward <acward96@gmail.com>
2019-03-21 16:26:38 -0400

    stage before fix

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,11 @@
-
 mod lib;
 fn main() {
-    let distances: Vec<u32> = vec![
-        0, 2, 9, 0,
-        1, 0, 6, 4,
-        0, 7, 0, 8,
-        6, 3, 0, 0
-    ];
+    let distances: Vec<u32> = vec![0, 2, 9, 0, 1, 0, 6, 4, 0, 7, 0, 8, 6, 3, 0, 0];
     let w: lib::DistanceMatrix = lib::DistanceMatrix::new(4, distances);
     lib::travel(w);
     let distances2: Vec<u32> = vec![
-        0, 1, 0, 1, 5,
-        9, 0, 3, 2, 0,
-        0, 0, 0, 4, 0,
-        0, 0, 2, 0, 3,
-        3, 0, 0, 0, 0
+        0, 1, 0, 1, 5, 9, 0, 3, 2, 0, 0, 0, 0, 4, 0, 0, 0, 2, 0, 3, 3, 0, 0, 0, 0,
     ];
     let w2: lib::DistanceMatrix = lib::DistanceMatrix::new(5, distances2);
     lib::travel(w2);
-
 }
