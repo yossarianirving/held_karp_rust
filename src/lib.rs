d212324f497bdea305cfdb05add37d81d4e25572
Chris Ward <acward96@gmail.com>
2019-03-29 18:28:43 -0400

    Removed parallel function

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
 extern crate itertools;
-extern crate rayon;
 
 use itertools::Itertools;
-use rayon::prelude::*;
 
 use std::collections::HashMap;
 
@@ -74,9 +72,9 @@ pub fn travel(w_array: DistanceMatrix) -> (u32, Vec<usize>) {
         let comb_by_len = (1..w_array.size).combinations(set_len).collect_vec();
         //        println!("{:?}", comb_by_len);
         // iterate through all combinations of a given length
-        let new_cols: Vec<(Vec<usize>, Vec<(usize, Option<u32>)>)> = comb_by_len
+        comb_by_len
             .iter()
-            .map(|comb| {
+            .for_each(|comb| {
             let mut dist: Vec<(usize, Option<u32>)> = vec![(0, None); w_array.size];
             // for each possible value
             (1..w_array.size)
@@ -109,131 +107,9 @@ pub fn travel(w_array: DistanceMatrix) -> (u32, Vec<usize>) {
                     };
                 });
             // println!("{:?}, {:?}", comb, dist);
-            // dist_map.insert(comb.clone(), dist);
-            (comb.clone(), dist)
-        }).collect();
-        new_cols.iter().for_each(|i| {
-            dist_map.insert(i.0.clone(), i.1.clone());
+            dist_map.insert(comb.clone(), dist);
         });
-    }
-    let mut comb: Vec<usize> = (1..w_array.size).collect();
-    let last_val: (usize, Option<u32>) = comb
-        .iter()
-        .map(|k| {
-            let w = w_array.get(0, *k);
-            // gets the correct column for A - Vx
-            let d_col: Vec<usize> = comb.iter().filter(|x| *x != k).cloned().collect();
-            // gets the value of D[Vx][A - Vx]
-            let d_val: Option<u32> = dist_map[&d_col][*k].1;
-            // add the two together
-            let val: Option<u32> = match (w, d_val) {
-                (Some(x), Some(y)) => Some(x + y), // if both are not infinity, add them
-                (_, _) => None,                    // else (at least one is infinity, retrn none
-            };
-            (*k, val)
-        })
-        .filter(|i| i.1.is_some())
-        .min_by_key(|i| i.1)
-        .unwrap();
-    let mut trip: Vec<usize> = vec![0];
-    // calculate minimum trip
-    // set next city
-    let mut next: usize = last_val.0;
-    trip.push(next);
-    comb = comb.iter().filter(|i| **i != next).cloned().collect();
-    while !comb.is_empty() {
-        next = dist_map[&comb][next].0;
-        trip.push(next);
-        comb = comb.iter().filter(|i| **i != next).cloned().collect();
-    }
-    trip.push(0);
-    // let last_comb: Vec<usize> = (1..w_array.size).collect();
-    // println!("{:?}: [({}, {:?})]", last_comb, last_val.0, last_val.1);
-    // println!("Trip: {:?}", trip);
-    // println!("Distance: {}", last_val.1.unwrap());
-    (last_val.1.unwrap(), trip)
-}
 
-/// ### Parallel Asymmetric Traveling Salesperson Problem
-/// 
-/// This function impliments a parallelized version of the
-/// Held–Karp algorithm using the Rayon crate. 
-/// Note that the performance benefits of using
-/// the parallel version compared to the sequential
-/// are only apparent with more than 11 cities.
-/// 
-/// # Examples
-/// 
-/// This example comes from the wikipedia page for the 
-/// [Held-Karp algorithm](https://en.wikipedia.org/wiki/Held%E2%80%93Karp_algorithm#Example[4])
-/// 
-/// ```
-/// let distances: Vec<u32> = vec! [
-///     0, 2, 9, 10,
-///     1, 0, 6, 4,
-///     15, 7, 0, 8,
-///     6, 3, 12, 0
-/// ];
-/// let dist_matrix = held_karp_rust::DistanceMatrix::new(4, distances);
-/// let result = held_karp_rust::par_travel(dist_matrix);
-/// assert_eq!(result.0, 21);
-/// assert_eq!(result.1, vec![0, 2, 3, 1, 0]);
-/// ```
-
-pub fn par_travel(w_array: DistanceMatrix) -> (u32, Vec<usize>) {
-    // create the hash map
-    let mut dist_map: HashMap<Vec<usize>, Vec<(usize, Option<u32>)>> = HashMap::new();
-    //   calculate the void column
-    let void_col: Vec<(usize, Option<u32>)> = (0..w_array.size)
-        .map(|i: usize| (0, w_array.get(i, 0)))
-        .collect();
-    // println!("[], {:?}", void_col);
-    // add empty set column to distance map
-    dist_map.insert(Vec::new(), void_col);
-    // for each combination size
-    for set_len in 1..(w_array.size - 1) {
-        // combinations of length set_len
-        let comb_by_len = (1..w_array.size).combinations(set_len).collect_vec();
-        //        println!("{:?}", comb_by_len);
-        // iterate through all combinations of a given length
-        let new_cols: Vec<(Vec<usize>, Vec<(usize, Option<u32>)>)> = comb_by_len
-            .par_iter()
-            .map(|comb| {
-            let mut dist: Vec<(usize, Option<u32>)> = vec![(0, None); w_array.size];
-            // for each possible value
-            (1..w_array.size)
-                // filter out all values if it's contained in the the combination
-                .filter(|i| !comb.contains(i))
-                .for_each(|i| {
-                     let min_val:  Option<(usize, Option<u32>)> = comb
-                        .iter()
-                        .map(|k| {
-                            let w = w_array.get(i, *k);
-                            // gets the correct column for A - Vx
-                            let d_col: Vec<usize> =
-                                comb.iter().filter(|x| *x != k).cloned().collect();
-                            // gets the value of D[Vx][A - Vx]
-                            let d_val: Option<u32> = dist_map[&d_col][*k].1;
-                            // add the two together
-                            let val: Option<u32> = match (w, d_val) {
-                                (Some(x), Some(y)) => Some(x + y), // if both are not infinity, add them
-                                (_, _) => None, // else (at least one is infinity, retrn none
-                            };
-                            (*k, val)
-                        })
-                        .filter(|i| i.1.is_some())
-                        .min_by_key(|i| i.1);
-                    dist[i] = match min_val {
-                        None => (0, None),
-                        Some(x) => x,
-                    };
-                });
-
-            (comb.clone(), dist)
-        }).collect();
-        new_cols.iter().for_each(|i| {
-            dist_map.insert(i.0.clone(), i.1.clone());
-        });
     }
     let mut comb: Vec<usize> = (1..w_array.size).collect();
     let last_val: (usize, Option<u32>) = comb
@@ -304,35 +180,6 @@ mod tests {
         ];
         let w: DistanceMatrix = DistanceMatrix::new(5, distances);
         let result = travel(w);
-        assert_eq!(result.0, 14);
-        assert_eq!(result.1, vec![0, 1, 2, 3, 4, 0]);
-    }
-
-    #[test]
-    fn par_four_d() {
-        let distances: Vec<u32> = vec![
-            0, 2, 9, 0, 
-            1, 0, 6, 4, 
-            0, 7, 0, 8, 
-            6, 3, 0, 0
-        ];
-        let w: DistanceMatrix = DistanceMatrix::new(4, distances);
-        let result = par_travel(w);
-        assert_eq!(result.0, 21);
-        assert_eq!(result.1, vec![0, 2, 3, 1, 0]);
-    }
-
-    #[test]
-    fn par_five_d() {
-        let distances: Vec<u32> = vec![
-            0, 1, 0, 1, 5, 
-            9, 0, 3, 2, 0, 
-            0, 0, 0, 4, 0, 
-            0, 0, 2, 0, 3, 
-            3, 0, 0, 0, 0,
-        ];
-        let w: DistanceMatrix = DistanceMatrix::new(5, distances);
-        let result = par_travel(w);
         assert_eq!(result.0, 14);
         assert_eq!(result.1, vec![0, 1, 2, 3, 4, 0]);
     }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
 mod lib;
-extern crate rand;
-use rand::Rng;
-use std::time::Instant;
 fn main() {
     let distances: Vec<u32> = vec![0, 2, 9, 0, 1, 0, 6, 4, 0, 7, 0, 8, 6, 3, 0, 0];
     let w: lib::DistanceMatrix = lib::DistanceMatrix::new(4, distances);
@@ -11,14 +8,4 @@ fn main() {
     ];
     let w2: lib::DistanceMatrix = lib::DistanceMatrix::new(5, distances2);
     lib::travel(w2);
-    let n: usize = 20;
-    let mut rng = rand::thread_rng();
-    let distances3: Vec<u32> = (0..(n * n)).map(|_| rng.gen_range(0, 18)).collect();
-    let w3: lib::DistanceMatrix = lib::DistanceMatrix::new(n, distances3);
-    let mut now = Instant::now();
-    lib::travel(w3.clone());
-    println!("Non-parallel {}: {}ms", n, now.elapsed().as_millis());
-    now = Instant::now();
-    lib::par_travel(w3);
-    println!("Parallel {}: {}ms", n, now.elapsed().as_millis());
 }
